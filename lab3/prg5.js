8d02a99e6e61d3ad2389c81c9709e96e16fd591c
adarsh-rajput01 <adarsh.25b01010177@abes.ac.in>
2026-09-15 14:30:42 +0530

    prg6

--- lab3/prg5.js
+++ lab3/prg5.js
@@ -1,20 +1,29 @@
-import https from 'https'
+import http from "http";
 
-const server = https.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+  //req method -> GET, POST, PUT, DELETE, PATCH
+  console.log("Method:", req.method);
+  console.log("URL:", req.url);
 
-    //req method -> GET, POST, PUT, DELETE, PATCH
-    console.log('Method:', req.method);
-    console.log('URL:', req.url);
+  // Browser can send only GET request to the server
+  // POST/PUT/PATCH/DELETE -> can be checked by api tester
+  // API Tester:- postman, echo api , thunder client
 
-    // Browser can send only GET request to the server 
-    // POST/PUT/PATCH/DELETE -> can be checked by api tester
-    // API Tester:- postman, echo api , thunder client 
-    
-    if (re.url === "/") {
-        res.setHeader("Content-Type", "text/html");
-        res.end("<h1>Welcome to my server</h1>");
-    }
-
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Welcome to my server</h1>");
+  }
+  else if (req.url === "/products") {
+    const products = [
+      { id: 1, name: "bottle"},
+      { id: 2, name: "laptop"},
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  }
+  else {
+    res.end();  
+  }
 });
 
 server.listen(5000, () => console.log("prg5 is running"));

0bed598a82705b3469dcfed95a3f0464c704992e
adarsh-rajput01 <adarsh.25b01010177@abes.ac.in>
2026-09-15 14:47:28 +0530

    prg 6

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,10 +1,26 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
+  if (req.url === "/" && req.method === "GET") {
     res.statusCode = 200;
-    res.end("sent with 200");
-  } else {
+    res.end("GET Request");
+  }
+  else if (req.url === "/" && req.method === "POST") {
+    res.statusCode = 200;
+    res.end("POST Request");
+  }
+
+  else if (req.url === "/" && req.method === "PUT") {
+    res.statusCode = 200;
+    res.end("PUT Request");
+  }
+
+  else if (req.url === "/" && req.method === "DELETE") {
+    res.statusCode = 200;
+    res.end("DELETE Request");
+  }
+  
+  else {
     res.statusCode = 404;
     res.end("request not found");
   }

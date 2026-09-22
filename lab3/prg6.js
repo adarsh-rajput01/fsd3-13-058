b4e170b855c0accef4b732b9fcaf6cacb9c1c519
adarsh-rajput01 <adarsh.25b01010177@abes.ac.in>
2026-09-22 14:16:57 +0530

    get all product completeed

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,46 +1,51 @@
 import http from "http";
+import { getAllProducts } from "./products.js";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
-    res.end("GET Request");
-  }
-  else if (req.url === "/" && req.method === "POST") {
+    const data = getAllProducts();
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        count: data.length,
+        data,
+      }),
+    );
+  } else if (req.url === "/api/v1/products" && req.method === "POST") {
     let body = "";
+
     req.on("data", (chunk) => {
       body += chunk;
     });
+
     req.on("end", () => {
-      const product = JSON.parse(body);
+      const product = JSON.parse(body || "{}");
       console.log("Received Product:", product);
-      res.statusCode = 200;
+      res.statusCode = 201;
+      res.setHeader("Content-Type", "application/json");
       res.end(JSON.stringify({ message: "product added", product }));
     });
-  }
-  else if (req.url.startsWith("/products/") && req.method === "PUT") {
+  } else if (req.url.startsWith("/api/v1/products/") && req.method === "PUT") {
     const productId = req.url.split("/").pop();
     console.log("Updating Product with ID:", productId);
+
     let body = "";
     req.on("data", (chunk) => {
       body += chunk;
     });
-    req.on("end", () => {
-      const products = JSON.parse(body);
-      products.id = productId;
-      res.statusCode = 200;
-      res.end(JSON.stringify({ message: "product updated", products }));
-    });
-  
-    res.statusCode = 200;
-    res.end("PUT Request");
-  }
 
-  else if (req.url === "/" && req.method === "DELETE") {
+    req.on("end", () => {
+      const product = JSON.parse(body || "{}");
+      product.id = productId;
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ message: "product updated", product }));
+    });
+  } else if (req.url === "/api/v1/products" && req.method === "DELETE") {
     res.statusCode = 200;
     res.end("DELETE Request");
-  }
-  
-  else {
+  } else {
     res.statusCode = 404;
     res.end("request not found");
   }
---
+++ lab3/products.js
@@ -0,0 +1,13 @@
+const products = [
+    { id :1,name:'marker', qty: 100, price: 15},
+    { id :2,name:'pen', qty: 200, price: 10},
+]
+
+let nextId = 3;
+
+export const getAllProducts = (req, res) => {
+    return products;
+}
+
+export const addProdcut = () => {
+    
